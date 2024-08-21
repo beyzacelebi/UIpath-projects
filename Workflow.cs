3489b61b785a08111046e3505198ea066e0aa6a7
rosi.e <99603158+beyzacelebi@users.noreply.github.com>
2024-08-21 13:05:03 +0300

    Add files via upload

---
+++ Workflow.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UiPath.CodedWorkflows;
+
+namespace workflowflowchart
+{
+    public class Workflow : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+        }
+    }
+}
